eff961b14b2a85c4a407810d1d7e264dc1e918b9
Cristyan Valera <numenor21@gmail.com>
2020-12-27 22:05:14 -0400

    wip

--- resources/views/livewire/plans-table.blade.php
+++ resources/views/livewire/plans-table.blade.php
@@ -31,8 +31,7 @@
             </x-select>
         </div>
         <div class="pt-2 relative mx-1">
-            <button wire:click="resetFilters" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150"
-            >x</button>
+            <x-button wire:click="resetFilters">x</x-button>
         </div>
     </div>
     @if($plans->isNotEmpty())
